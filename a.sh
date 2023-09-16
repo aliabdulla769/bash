3978e8094f44e335999278d87d4b3f448d75fc39
aliabdulla769 <131328611+aliabdulla769@users.noreply.github.com>
2023-09-16 16:31:51 +0300

    Create a.sh

---
+++ a.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+while true; do
+  # Start xmrig
+  ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u TRX:TUPkoHaVv8jp6CbsQGjd8bhDZXBVxkQkSt.gt -p x &
+
+  # Sleep for 30 seconds
+  sleep 300
+
+  # Stop xmrig
+  pkill xmrig
+
+  # Sleep for a few seconds before restarting (adjust as needed)
+  sleep 5
+done
