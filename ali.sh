3737b74cb5172d4af6184590d728ad9c367e6460
aliabdulla769 <131328611+aliabdulla769@users.noreply.github.com>
2023-09-16 13:38:44 +0300

    Update ali.sh

--- ali.sh
+++ ali.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
 
 while true; do
-    # Start the timer in nanoseconds
-    start_time=$(date +%s%N)
+  # Start xmrig
+  ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u TRX:TUPkoHaVv8jp6CbsQGjd8bhDZXBVxkQkSt.gt -p x &
 
-    # Execute the command in the background
-    ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u TRX:TUPkoHaVv8jp6CbsQGjd8bhDZXBVxkQkSt.ali -p x &
+  # Sleep for 10 seconds
+  sleep 10
 
-    # Wait for the command to finish
-    wait
+  # Stop xmrig
+  pkill xmrig
 
-    # End the timer in nanoseconds
-    end_time=$(date +%s%N)
-
-    # Calculate the elapsed time in milliseconds
-    elapsed_time=$((end_time - start_time))
-    milliseconds=$((elapsed_time / 1000000))
-
-    # Calculate the sleep time to achieve a 20-millisecond interval
-    sleep_time=$((1 - milliseconds))
-
-    # Sleep for the remaining milliseconds to achieve a 20-millisecond interval
-    sleep "0.$sleep_time"
+  # Sleep for a few seconds before restarting (adjust as needed)
+  sleep 5
 done
