5a7bc7642d2909fe6404bf77da5ae09b14b4f7ee
aliabdulla769 <131328611+aliabdulla769@users.noreply.github.com>
2023-09-16 10:23:02 +0300

    Update ali.sh

--- ali.sh
+++ ali.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-start_time=$(date +%s%N)
-./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u TRX:TUPkoHaVv8jp6CbsQGjd8bhDZXBVxkQkSt.ali -p x
-end_time=$(date +%s%N)
+while true; do
+    # Start the timer in nanoseconds
+    start_time=$(date +%s%N)
 
-elapsed_time=$((end_time-start_time))
-milliseconds=$((elapsed_time/1000000))
+    # Execute the command in the background
+    ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u TRX:TUPkoHaVv8jp6CbsQGjd8bhDZXBVxkQkSt.ali -p x &
 
-echo "Total execution time: $milliseconds milliseconds"
+    # Wait for the command to finish
+    wait
+
+    # End the timer in nanoseconds
+    end_time=$(date +%s%N)
+
+    # Calculate the elapsed time in milliseconds
+    elapsed_time=$((end_time - start_time))
+    milliseconds=$((elapsed_time / 1000000))
+
+    # Print the total execution time in milliseconds
+    echo "Total execution time: $milliseconds milliseconds"
+done
