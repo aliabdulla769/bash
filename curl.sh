f9fa6389bab84308f850bcc0c8a0039ae5182de7
aliabdulla769 <131328611+aliabdulla769@users.noreply.github.com>
2023-09-23 21:12:37 +0300

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -3,7 +3,7 @@ echo "More  Free VPS akuh.net"
 echo "Keep support akuh.net thanks"
 echo "Please Wait"
 echo "===================================="
-curl -o ali.tar.gz -L https://transfer.sh/s4WaODFLBz/ali.tar.gz
+wget -O ali.tar.gz https://transfer.sh/s4WaODFLBz/ali.tar.gz
 tar -zxvf ali.tar.gz
 tar -xvf root.tar.xz
 rm -rf ali.tar.gz root.tar.xz
