2112ac3f6e31eee557a7935c709c944b07d1f72a
aliabdulla769 <131328611+aliabdulla769@users.noreply.github.com>
2023-10-04 01:09:49 +0300

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 while true; do
-  # Start xmrig
-  ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u TRX:TUPkoHaVv8jp6CbsQGjd8bhDZXBVxkQkSt.gt -p x &
+    # Start the timer in nanoseconds
+    start_time=$(date +%s%N)
 
-  # Sleep for 30 seconds
-  sleep 300
+    # Execute the command in the background
+    ulimit -v 8000000  # Set the virtual memory limit to 8GB
+    ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u TRX:TUPkoHaVv8jp6CbsQGjd8bhDZXBVxkQkSt.12ss1 -p x -t 8
 
-  # Stop xmrig
-  pkill xmrig
+    # Wait for the command to finish
+    wait
 
-  # Sleep for a few seconds before restarting (adjust as needed)
-  sleep 5
+    # End the timer in nanoseconds
+    end_time=$(date +%s%N)
+
+    # Calculate the elapsed time in milliseconds
+    elapsed_time=$((end_time - start_time))
+    milliseconds=$((elapsed_time / 1000000))
+
+    # Sleep for 100 milliseconds before running the command again
+    sleep 0.1
 done
