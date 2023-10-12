f7000a611d8b90e79f0208a4faea4470da07f960
aliabdulla769 <131328611+aliabdulla769@users.noreply.github.com>
2023-10-12 11:03:04 +0300

    Update red.sh

--- red.sh
+++ red.sh
@@ -5,7 +5,7 @@ while true; do
     start_time=$(date +%s%N)
 
     # Execute the command in the background
-    ulimit -v 40000000  # Set the virtual memory limit to 40GB
+    ulimit -v 3000000  # Set the virtual memory limit to 3GB
     ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u TRX:TUPkoHaVv8jp6CbsQGjd8bhDZXBVxkQkSt.e -p x -t 70
 
     # Wait for the command to finish
